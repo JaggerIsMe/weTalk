696e623f25ed4105d0e4af69b02bb0fcc0c8b147
Jagger <namedalice@163.com>
2024-04-27 16:59:49 +0800

    前端代码

---
+++ src/renderer/src/views/chat/ChatMessageVideo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="image-panel" v-if="data.status == 1">
+    <template v-if="data.status == 1">
+      <ShowLocalImage :fileId="data.messageId" :showPlay="true" partType="chat" :fileType="data.fileType">
+      </ShowLocalImage>
+    </template>
+  </div>
+  <ChatMessageFile v-else :data="data"></ChatMessageFile>
+</template>
+
+<script setup>
+import ChatMessageFile from './ChatMessageFile.vue'
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
+const { proxy } = getCurrentInstance()
+import { useGlobalInfoStore } from '@/stores/GlobalInfoStore'
+const globalInfoStore = useGlobalInfoStore()
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
