0ad4c7c1ea3b305091bb3766e0c864ed9472dcd3
Jagger <namedalice@163.com>
2024-05-09 01:06:11 +0800

    修复管理员能强制自己下线和更改自己账号状态的BUG

--- src/main/java/com/weTalk/controller/AdminController.java
+++ src/main/java/com/weTalk/controller/AdminController.java
@@ -1,14 +1,17 @@
 package com.weTalk.controller;
 
 import com.weTalk.annotation.GlobalInterceptor;
+import com.weTalk.dto.TokenUserInfoDto;
 import com.weTalk.entity.query.UserInfoQuery;
 import com.weTalk.entity.vo.PaginationResultVO;
 import com.weTalk.entity.vo.ResponseVO;
+import com.weTalk.exception.BusinessException;
 import com.weTalk.service.UserInfoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -35,25 +38,37 @@ public class AdminController extends ABaseController {
 
     /**
      * 更新用户状态
+     *
+     * @param request
      * @param status
      * @param userId
      * @return
      */
     @RequestMapping("/updateUserStatus")
     @GlobalInterceptor(checkAdmin = true)
-    public ResponseVO updateUserStatus(@NotNull Integer status, @NotEmpty String userId) {
+    public ResponseVO updateUserStatus(HttpServletRequest request, @NotNull Integer status, @NotEmpty String userId) {
+        TokenUserInfoDto adminUserDto = getTokenUserInfo(request);
+        if (adminUserDto.getUserId().equals(userId)) {
+            throw new BusinessException("不能更改自己的账号状态");
+        }
         userInfoService.updateUserStatus(status, userId);
         return getSuccessResponseVO(null);
     }
 
     /**
      * 强制用户下线
+     *
+     * @param request
      * @param userId
      * @return
      */
     @RequestMapping("/forceOffLine")
     @GlobalInterceptor(checkAdmin = true)
-    public ResponseVO forceOffLine(@NotEmpty String userId) {
+    public ResponseVO forceOffLine(HttpServletRequest request, @NotEmpty String userId) {
+        TokenUserInfoDto adminUserDto = getTokenUserInfo(request);
+        if (adminUserDto.getUserId().equals(userId)) {
+            throw new BusinessException("不能强制自己下线");
+        }
         userInfoService.forceOffLine(userId);
         return getSuccessResponseVO(null);
     }
